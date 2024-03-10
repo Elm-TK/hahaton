bbfbb9ec6e340373327224389ac431d10f7d2ee2
yam501 <ya.misha501@gmail.com>
2024-03-10 22:34:19 +0500

    чудо select

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -5,34 +5,48 @@ import {
 import {useState} from "react";
 
 const Filters = () => {
-    const [value, setValue] = useState<string>('Категория')
+    const [value, setValue] = useState<string>("Категория")
+
     let locationOptions: Map<string, any> = new Map([
-        ['ключ1', new Map<string, string>([['ключ11', 'значение11'], ['ключ12', 'значение12']])],
+        ['ключ1', new Map<string, any>([['ключ11', new Map<string, any>([['ключ111', 'значение111'], ['ключ112', 'значение112']])], ['ключ12', 'значение12']])],
         ['ключ2', 'значение2'],
         ['ключ3', 'значение3']
     ])
+
+    const [currMap, setCurrMap] = useState<Map>(locationOptions)
+
+
     const e = (k: string) => {
-        const c = locationOptions.get(k)
+        const c = currMap.get(k)
         let f: any = []
         if (typeof c === 'string') {
-            f.push(<option key={c}>{c}</option>)
+            f.push(<option value={k}  key={c}>{c}</option>)
         } else {
             for (const kElement of c) {
-                f.push(<option key={kElement[0]}>{kElement[0]}</option>)
+                f.push(<option value={k} key={kElement[0]}>{kElement[0]}</option>)
             }
         }
         return f
     }
-    const d = () => Array.from(locationOptions.keys()).map(k => (
+
+    const changeLocation = (e) => {
+        setValue(e.target.value)
+        setCurrMap(currMap.get(e.target.value))
+
+    }
+
+    const d = (a) => Array.from(a.keys()).map(k => (
         <optgroup label={k} key={k}>
             {e(k)}
         </optgroup>
     ))
     return (
         <div className="flex mb-10 bg-[#00AAFF]">
-            <select className="w-96" value={value} onChange={(e) => setValue(e.currentTarget.value)}>
+            <select className="w-96" value={value} onChange={(e) => changeLocation(e)}>
+
                 <option disabled>Категория</option>
-                {d()}
+                <option>{value}</option>
+                {d(currMap)}
             </select>
             <Button variant="contained"
                     style={{
