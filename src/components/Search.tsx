1614b589bd32300fb036bbd0a49af0f1e15cf0a5
ETK <temirlan200370@email.com>
2024-03-11 18:51:40 +0500

    add 3 autocomplite

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,33 @@
 import {Autocomplete, TextField} from "@mui/material";
 import API_Service from "../API/API_Service.ts";
+import {useEffect} from "react";
+import Button from "@mui/material/Button";
 
 const Search = () => {
-    const autocompleteOptions = API_Service.getForSearch()
-    const categories :string[] = []
-    const ids :number[] = []
-    const locations :string[] = []
-    let out; autocompleteOptions.then(x => out = x); console.log(out)
-    // autocompleteOptions.forEach((row) => {
-    //     ids.push(row.id)
-    //     categories.push(row.category)
-    //     locations.push(row.location)
-    // })
+    // const autocompleteOptions = API_Service.getForSearch()
+    let categories :string[] = []
+    let ids :number[] = []
+    let locations :string[] = []
+    async function fetch() {
+        const res =  await API_Service.getForSearch()
+        let cat :string[] = []
+        let i :number[] = []
+        let l :string[] = []
+        res.forEach((row) => {
+            ids.push(row.id)
+            categories.push(row.category)
+            locations.push(row.location)
+        })
+        categories = cat
+        ids = i
+        locations = l
+    }
+    useEffect(() => {
+        fetch()
+    },[])
+
     return (
-        <div>
+        <div className="flex">
             <Autocomplete
                 noOptionsText={'Такой категории нет'}
                 className="mr-5"
@@ -37,6 +51,59 @@ const Search = () => {
                     />
                 }
             />
+            <Autocomplete
+                noOptionsText={'Такой категории нет'}
+                className="mr-5"
+                options={categories}
+                sx={{width: "100%"}}
+                // ref={autoCompleteRef}
+                ListboxProps={{style: {maxHeight: 300}}}
+                onKeyDown={(event) => {
+                    event.defaultMuiPrevented = true;
+                    if (event.key === 'Enter') {
+                        // checkAnswer()
+                    }
+                }}
+                renderInput={(params) =>
+                    <TextField
+                        {...params}
+                        // inputRef={valueRef}
+                        // error={isError}
+                        // helperText={isError ? helperText : ""}
+                        label="Country"
+                    />
+                }
+            />
+            <Autocomplete
+                noOptionsText={'Такой категории нет'}
+                className="mr-5"
+                options={locations}
+                sx={{width: "100%"}}
+                // ref={autoCompleteRef}
+                ListboxProps={{style: {maxHeight: 300}}}
+                onKeyDown={(event) => {
+                    event.defaultMuiPrevented = true;
+                    if (event.key === 'Enter') {
+                        // checkAnswer()
+                    }
+                }}
+                renderInput={(params) =>
+                    <TextField
+                        {...params}
+                        // inputRef={valueRef}
+                        // error={isError}
+                        // helperText={isError ? helperText : ""}
+                        label="Country"
+                    />
+                }
+            />
+            <Button variant="contained"
+                    style={{
+                        backgroundColor: "#00AAFF",
+                        color: "#000000",
+                        // padding: "18px 36px"
+                    }}
+            >Найти</Button>
         </div>
     );
 };
